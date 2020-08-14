52bfd2583665fcaa144dd5bb3a8cb5f82c7787f5
Bee <pizzaman8099@gmail.com>
2020-08-14 13:31:51 -0700

    Build out main function

--- polymonitor/polymonitor.py
+++ polymonitor/polymonitor.py
@@ -1,5 +1,37 @@
 #!/usr/bin/env python3
+
 import argparse as ap
+from functools import reduce
+from requests import get, codes
+from requests.exceptions import MissingSchema
+
+
+def make_value_dict(is_symbolic):
+    map_to_res = {
+        True: 'f062'.decode('hex') if is_symbolic else "Up",
+        False: 'f98d'.decode('hex') if is_symbolic else "Down"
+    }
+
+    return map_to_res
+
+
+def get_status_code(url):
+    """
+    Gets the current status code of a full url or short url. Short urls will
+    be prepended with https.
+    """
+    try:
+        return get(url).status_code
+    except MissingSchema:
+        return get_status_code("https://" + url)
+
+
+def count_stats(acc, is_ok):
+    """
+    Increment pair counter based on value of boolean.
+    """
+    n_ok, n_fail = acc
+    return (n_ok + 1, n_fail) if is_ok else (n_ok, n_fail + 1)
 
 
 def main():
@@ -11,11 +43,6 @@ def main():
         help='Displays the results as symbols',
         action='store_true')
     parser.add_argument(
-        '-t',
-        '--text',
-        help='Displays the results as readable text',
-        action='store_true')
-    parser.add_argument(
         '-c',
         '--compact',
         help='Reduces the results into a more compact package',
@@ -24,12 +51,33 @@ def main():
         '-u',
         '--urls',
         help='Pass in URLs to monitor',
+        nargs='+',
+        type=str,
         action='extend')
 
     args = parser.parse_args()
+    val_dict = make_value_dict(args.symbolic)
+    res = ""
 
-    # res = ""
-    print(args)
+    if args.urls is not None:
+        stats = map(lambda url: (url, get_status_code(url) == codes.ok),
+                    args.urls)
+        if args.compact:
+            failed = list(filter(lambda url_pr: not url_pr[1], stats))
+            count = reduce(count_stats, map(lambda url_pr: url_pr[1], stats))
+            count_str = f"{val_dict[True]}: {count[0]}"
+            failed_str = f" {val_dict[False]}: {failed.join(' ')}"
+            res = count_str + failed_str
+        else:
+            res = list(
+                map(lambda url_pr: f"{url_pr[0]}: {val_dict(url_pr[1])}"))
+
+        res = list(stats)
+    else:
+        res = "Please pass in valid urls you would like to monitor"
+
+    print(res)
+    return res
 
 
 if __name__ == '__main__':
--- tests/test_polymonitor.py
+++ tests/test_polymonitor.py
@@ -1,9 +1,27 @@
-from polymonitor import __version__, main
+import unittest
+from polymonitor.polymonitor import get_status_code
+from requests import codes
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+class TestPolyMonitor(unittest.TestCase):
+    def test_good_get_status_code(self):
+        """
+        Test that it returns an OK status code for known working urls.
+        """
+        assert get_status_code('google.com') == codes.ok
+
+    def test_bad_get_status_code(self):
+        """
+        Test that it returns a bad status code for known working urls.
+        """
+        assert get_status_code('google.com/404') != codes.ok
+
+    def test_long_url_get_status_code(self):
+        """
+        Test that it returns an OK status code for known working urls.
+        """
+        assert get_status_code('https://google.com') == codes.ok
 
 
-def test_returns_string():
-    assert type(main()) is str
+if __name__ == '__main__':
+    unittest.main()
